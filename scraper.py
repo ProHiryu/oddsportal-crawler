e0f00c6bb5596dc6027b1ac55ffbe81c6a88ece9
ProHiryu <songheqi1996@gmail.com>
2017-04-16 12:56:47 +0800

    init commit

--- scraper.py
+++ scraper.py
@@ -70,44 +70,7 @@ print(len(team), len(odd1), len(odd2), len(result))
 
 for i in range(len(team)):
     cur.execute('''INSERT INTO lpl_2017_spring (team,odd1,odd2,result) VALUES (?,?,?,?)''',
-               (team[i], odd1[i], odd2[i], result[i]))
-
-# for i in range(5):
-#     table = driver.find_element_by_tag_name('table')
-#     table_rows = table.find_elements_by_tag_name('tr')
-#     for tr in table_rows:
-#         td = tr.find_elements_by_tag_name('td')
-#         row = [i.text for i in td]
-#         if len(row) == 0:
-#             continue
-#         cur.execute('''INSERT INTO Player (name, team, role, show_times, kda, contribution,
-#             kill_avg, kill_most, dead_avg, dead_most, assist_avg) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )''',
-#             ( row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11] ) )
-#         print(row)
-#
-#     page_button = driver.find_element_by_xpath('html/body/div[2]/div[2]/div[1]/div[2]')
-#     page_button.click()
-#     time.sleep(1)
-#
-#     table = driver.find_element_by_tag_name('table')
-#     table_rows = table.find_elements_by_tag_name('tr')
-#     for tr in table_rows:
-#         td = tr.find_elements_by_tag_name('td')
-#         row = [i.text for i in td]
-#         if len(row) == 0:
-#             continue
-#         cur.execute('''UPDATE Player SET assist_most = ?, gpm = ?, cspm = ?, dpm = ?, damage = ?, apm = ?,
-#             afford = ?, wpm = ?,  exwpm = ? WHERE id = ?''',( row[2], row[3], row[4], row[5], row[6], row[7],
-#                                                               row[8], row[9], row[10], row[0] ))
-#         print(row)
-#
-#     page_button = driver.find_element_by_xpath('//*[@id="DataTables_Table_0_next"]')
-#     page_button.click()
-#     time.sleep(1)
-#
-#     page_button = driver.find_element_by_xpath('html/body/div[2]/div[2]/div[1]/div[1]')
-#     page_button.click()
-#     time.sleep(1)
+                (team[i], odd1[i], odd2[i], result[i]))
 
 conn.commit()
 conn.close()
